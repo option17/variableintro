d98f85d4de453d403796816cc547998b315dd7dd
Your Name <you@example.com>
2022-07-05 20:57:07 -0600

    Finished 2 and 3 knowledge check.

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ console.log(color);
 console.log(age)
 console.log(friends)
 
+// Knowledge Check part 2
+
 let user_points="17"
 
 if (user_points >=75){
-    console.log("Congratulations");
+    console.log("Congratulations!");
+} else if (user_points <50){
+    console.log("Just give up!")
 }
+    else{
+    console.log("Keep going you got this!");
+} 
