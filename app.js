b00b94ae04c6b6f227ec0ddfb7e7246d12f3fa7d
Your Name <you@example.com>
2022-07-07 16:54:42 -0600

    did part 2 of KC

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 let username="josh";
 let color="green";
-let age="23";
+let bestfriends="23";
 let friends="0";
 
 console.log(username);
 console.log(color);
-console.log(age)
-console.log(friends)
+
+console.log(bestfriends)
+
+// my practice of what we learnt so far.
+
+let myson="Henry"
+let old="6"
+
+console.log(myson);
+console.log(old);
+
+if (old =5){
+    console.log("Way to go my boy!")
+} else if (old <=5){
+    console.log("Still so cute my boy!")
+} if(old >=9){
+    console.log("Slow down my boy!")
+}
+
 
 // Knowledge Check part 2
 
-let user_points="17"
+let user_points="91"
+let is_member= true
+
+if (user_points >=90&&is_member){
+console.log("User is better than most at the game")}
+
+
+
 
 if (user_points >=75){
     console.log("Congratulations!");
@@ -20,3 +44,8 @@ if (user_points >=75){
     else{
     console.log("Keep going you got this!");
 } 
+
+// **W9A assignment**
+
+let age="23"
+let is_subscribed="47"
